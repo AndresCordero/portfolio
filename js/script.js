c5716ff51f71f80d60010d1393e2b453bd8c8cb6
Andres Cordero <an.cornmo@gmail.com>
2025-03-12 09:59:53 -0600

    styles

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-// Add custom JavaScript here
+function typingEffect() {
+    const text = 'I am Andres Cordero.'
+    const typingElement = document.getElementById('typing-text');
+    const typingDelay = 100;
+
+    typeText(text, typingElement, typingDelay);
+}
+
+function typeText(text, typingElement, delay) {
+    for (let i = 0; i < text.length; i++) {
+        setTimeout(() => {
+            typingElement.textContent += text.charAt(i);
+         }, delay * i);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', typingEffect);
