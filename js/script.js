c718ba9f4eb31ef975ccb7db5b7d16005a3cee11
Andres Cordero <an.cornmo@gmail.com>
2025-04-24 14:39:56 -0600

    Contact info

--- js/script.js
+++ js/script.js
@@ -10,8 +10,17 @@ function typeText(text, typingElement, delay) {
     for (let i = 0; i < text.length; i++) {
         setTimeout(() => {
             typingElement.textContent += text.charAt(i);
-         }, delay * i);
+        }, delay * i);
     }
 }
 
 document.addEventListener('DOMContentLoaded', typingEffect);
+
+
+/* Whatsapp funtion */
+function handleClick() {
+    const phone = '50688020529'; // replace with your WhatsApp number (no + or spaces)
+    const message = encodeURIComponent("Hi! I'm interested in your services.");
+    const url = `https://wa.me/${phone}?text=${message}`;
+    window.open(url, '_blank');
+}
